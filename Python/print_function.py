a8e2f8e54667375996706ac953e1468b3b86e54d
pavithraravi07 <pavithraravisskv@gmail.com>
2021-02-26 19:10:43 +0530

    folder for Python programs

---
+++ Python/print_function.py
@@ -0,0 +1,14 @@
+//The included code stub will read an integer, n, from STDIN.
+//Without using any string methods, try to print the following:
+//123...n
+//Note that "..." represents the consecutive values in between.
+
+//Input Format
+//The first line contains an integer nn.
+
+//Output Format
+//Print the list of integers from 1 through n as a string, without spaces.
+
+if __name__ == '__main__':
+    n = int(input())
+    print(*range(1,n+1),sep="")
