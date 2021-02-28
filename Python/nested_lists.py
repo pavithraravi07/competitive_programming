78aaf8363b6bc8595255c04cf103f110a60e7177
pavithraravi07 <pavithraravisskv@gmail.com>
2021-02-28 13:11:24 +0530

    folder for Python programs

--- Python/nested_lists.py
+++ Python/nested_lists.py
@@ -1,19 +1,19 @@
-//Given the names and grades for each student in a class of N students, store them in a nested list and print the name(s) of any student(s) 
-//having the second lowest grade.
+#Given the names and grades for each student in a class of N students, store them in a nested list and print the name(s) of any student(s) 
+#having the second lowest grade.
 
-//Note: If there are multiple students with the second lowest grade, order their names alphabetically and print each name on a new line.
+#Note: If there are multiple students with the second lowest grade, order their names alphabetically and print each name on a new line.
 
-//Input Format
+#Input Format
 
-//The first line contains an integer,N , the number of students.
-//The 2N subsequent lines describe each student over 2 lines.
-//- The first line contains a student's name.
-//- The second line contains their grade.
+#The first line contains an integer,N , the number of students.
+#The 2N subsequent lines describe each student over 2 lines.
+#- The first line contains a student's name.
+#- The second line contains their grade.
 
-//Output Format
+#Output Format
 
-//Print the name(s) of any student(s) having the second lowest grade in. If there are multiple students, order their names alphabetically 
-//and print each one on a new line.
+#Print the name(s) of any student(s) having the second lowest grade in. If there are multiple students, order their names alphabetically 
+#and print each one on a new line.
 
 if __name__ == '__main__':
     records = [[input(), float(input())] for _ in range(int(input()))]
