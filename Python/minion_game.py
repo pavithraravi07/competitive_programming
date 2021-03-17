7a33e26d1eeb4e8bc261b2289f39132a9d9b60f0
pavithraravi07 <pavithraravisskv@gmail.com>
2021-03-17 14:29:37 +0530

    folder for Python programs

---
+++ Python/minion_game.py
@@ -0,0 +1,60 @@
+#Kevin and Stuart want to play the 'The Minion Game'.
+
+#Game Rules
+
+#Both players are given the same string, S.
+#Both players have to make substrings using the letters of the string S.
+#Stuart has to make words starting with consonants.
+#Kevin has to make words starting with vowels.
+#The game ends when both players have made all possible substrings.
+
+#Scoring
+#A player gets +1 point for each occurrence of the substring in the string S.
+
+#For Example:
+#String S = BANANA
+#Kevin's vowel beginning word = ANA
+#Here, ANA occurs twice in BANANA. Hence, Kevin will get 2 Points.
+
+#Your task is to determine the winner of the game and their score.
+
+#Input Format
+
+#A single line of input containing the string S.
+#Note: The string  will contain only uppercase letters: [A - Z].
+
+#Output Format
+
+#Print one line: the name of the winner and their score separated by a space.
+
+#If the game is a draw, print Draw.
+
+#Sample Input
+
+#BANANA
+
+#Sample Output
+
+#Stuart 12
+
+def minion_game(string):
+    s = 0
+    k = 0
+    v = 'AEIOU'
+    l = len(string)
+    for i in range(l):
+        if string[i] in v:
+            k += l - i
+        else:
+            s += l - i
+    if s > k: 
+        print "Stuart", s
+    elif k >s:
+        print "Kevin", k
+    else:
+        print("Draw")
+            
+
+if __name__ == '__main__':
+    s = input()
+    minion_game(s
