11f83962be88fa4bb3ab09f783cb5c5b40a4b1bc
pavithraravi07 <pavithraravisskv@gmail.com>
2021-03-25 18:17:19 +0530

    folder for Python programs

---
+++ Python/calendar_module.py
@@ -0,0 +1,22 @@
+#Task
+
+#You are given a date. Your task is to find what the day is on that date.
+
+#Input Format
+
+#A single line of input containing the space separated month, day and year, respectively, in MM DD YYYY format.
+
+#Constraints
+
+#2000 < year < 3000
+
+#Output Format
+
+#Output the correct day in capital letters.
+
+
+import calendar
+
+month, day, year = map(int,input().split())
+correct_day = calendar.day_name[calendar.weekday(year, month, day)]
+print(correct_day.upper())
