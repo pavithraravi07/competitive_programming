e1e7db54bc08dc952ce92e1fc781bc4c6b32b265
pavithraravi07 <pavithraravisskv@gmail.com>
2021-04-19 21:41:58 +0530

    folder for Python programs

---
+++ Python/triangle_quest.py
@@ -0,0 +1,33 @@
+"""
+Task
+You are given a positive integer n.
+Your task is to print a palindromic triangle of size n.
+
+For example, a palindromic triangle of size 5 is:
+
+1
+121
+12321
+1234321
+123454321
+You can't take more than two lines. The first line (a for-statement) is already written for you.
+You have to complete the code using exactly one print statement.
+
+Note:
+Using anything related to strings will give a score of 0.
+Using more than one for-statement will give a score of 0.
+
+Input Format
+
+A single line of input containing the integer n.
+
+Constraints
+
+Output Format
+
+Print the palindromic triangle of size n as explained above.
+"""
+
+
+for i in range(1,int(input())): #More than 2 lines will result in 0 score. Do not leave a blank line also
+    print ((10**(i)//9)*i)
