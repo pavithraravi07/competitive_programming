92feaddc15ae6deffa252997c7494acde44647bf
pavithraravi07 <pavithraravisskv@gmail.com>
2021-05-02 19:29:35 +0530

    folder for Python programs

---
+++ Python/ginorts.py
@@ -0,0 +1,27 @@
+"""
+Task
+You are given a string S.
+S contains alphanumeric characters only.
+
+Your task is to sort the string S in the following manner:
+
+All sorted lowercase letters are ahead of uppercase letters.
+All sorted uppercase letters are ahead of digits.
+All sorted odd digits are ahead of sorted even digits.
+Input Format
+
+A single line of input contains the string S.
+
+Constraints
+
+0 < len(S) < 1000
+
+Output Format
+
+Output the sorted string S.
+"""
+
+
+import string
+
+print(*sorted(input(), key=(string.ascii_letters + '1357902468').index), sep='')
