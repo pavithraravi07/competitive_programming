40cce0cd486fb9192230a0671692eff54e78c1d3
pavithraravi07 <pavithraravisskv@gmail.com>
2021-05-07 18:06:53 +0530

    folder for Python programs

---
+++ Python/integers_comes_in_all_sizes.py
@@ -0,0 +1,13 @@
+#Task
+#Read four numbers, a, b, c, and d, and print the result of a^b + c^d.
+
+#Input Format
+#Integers a, b, c, and d are given on four separate lines, respectively.
+
+#Output Format
+#Print the result on one line.
+
+
+
+a, b, c, d = int(input()), int(input()), int(input()), int(input())
+print(pow(a,b)+pow(c,d))
--- Python/triangle_quest.py
+++ Python/triangle_quest.py
@@ -30,4 +30,4 @@ Print the palindromic triangle of size n as explained above.
 
 
 for i in range(1,int(input())): #More than 2 lines will result in 0 score. Do not leave a blank line also
-    print ((10**(i)//9)*i)
+    print ((10**(i)//9)*in)
