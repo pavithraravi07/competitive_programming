3275a5b6a38fe85a4411c6b643892d2d1f15c083
pavithraravi07 <pavithraravisskv@gmail.com>
2021-05-16 19:40:44 +0530

    folder for Python programs

---
+++ Python/complex_numbers.py
@@ -0,0 +1,55 @@
+"""
+
+Task
+For this challenge, you are given two complex numbers, and you have to print the result of their addition, subtraction, multiplication, division and modulus operations.
+
+The real and imaginary precision part should be correct up to two decimal places.
+
+Input Format
+
+One line of input: The real and imaginary part of a number separated by a space.
+
+Output Format
+
+For two complex numbers C and D, the output should be in the following sequence on separate lines:
+
+C + D
+C - D
+C * D
+C / D
+mod(C)
+mod(D)
+
+
+"""
+
+
+
+import math
+
+class Complex(complex):
+    def __add__(self, no):
+        return Complex(complex.__add__(self, no))
+
+    def __sub__(self, no):
+        return Complex(complex.__sub__(self, no))
+
+    def __mul__(self, no):
+        return Complex(complex.__mul__(self, no))
+
+    def __truediv__(self, no):
+        return Complex(complex.__truediv__(self, no))
+
+    def mod(self):
+        return Complex(complex.__abs__(self))
+
+    def __str__(self):
+        return '{0.real:.2f}{0.imag:+.2f}i'.format(self+0)
+        
+
+if __name__ == '__main__':
+    c = map(float, input().split())
+    d = map(float, input().split())
+    x = Complex(*c)
+    y = Complex(*d)
+    print(*map(str, [x+y, x-y, x*y, x/y, x.mod(), y.mod()]), sep='\n')
