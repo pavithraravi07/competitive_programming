6268e034350be72bebffef0e1c7c9d870e2e6587
pavithraravi07 <pavithraravisskv@gmail.com>
2021-06-03 11:55:44 +0530

    folder for Python programs

---
+++ Sql/weather_observation_station_12.sql
@@ -0,0 +1,10 @@
+/*
+Query the list of CITY names from STATION that do not start with vowels and do not end with vowels. 
+Your result cannot contain duplicates.
+*/
+
+
+
+select distinct city from station 
+where lower(substr(city,1,1)) not in ('a','e','i','o','u') 
+and lower(substr(city,-1)) not in ('a','e','i','o','u');
