815a82efa1eb5e436e2fa11446bf4d267a573373
pavithraravi07 <pavithraravisskv@gmail.com>
2021-06-18 16:19:55 +0530

    folder for Sql queries

---
+++ Sql/top_earners.sql
@@ -0,0 +1,17 @@
+/*
+We define an employee's total earnings to be their monthly salary x months worked, and 
+the maximum total earnings to be the maximum total earnings for any employee in the Employee table. 
+Write a query to find the maximum total earnings for all employees as well as the total number of employees 
+who have maximum total earnings. Then print these values as 2 space-separated integers.
+*/
+
+
+
+SELECT *
+FROM (
+    SELECT Months * Salary, COUNT(*)
+    FROM Employee
+    GROUP BY (Months * Salary)
+    ORDER BY Months * Salary DESC
+    )
+WHERE ROWNUM = 1;
