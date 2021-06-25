0853ad45ca35005a51d8c214ffd5880dbde4eab2
pavithraravi07 <pavithraravisskv@gmail.com>
2021-06-25 19:41:59 +0530

    folder for Sql queries

---
+++ Sql/population_census.sql
@@ -0,0 +1,11 @@
+/*
+Given the CITY and COUNTRY tables, query the sum of the populations of all cities where the CONTINENT is 'Asia'.
+
+Note: CITY.CountryCode and COUNTRY.Code are matching key columns.
+*/
+
+
+
+
+select sum(city.population) from city, country
+where city.countrycode = country.code and initcap(country.continent) = 'Asia';
