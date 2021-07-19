47195ccddef3745baff2a60f16509daab1eb1fef
pavithraravi07 <pavithraravisskv@gmail.com>
2021-07-19 14:44:22 +0530

    folder for Java programs

---
+++ Java/currency_formatter.java
@@ -0,0 +1,50 @@
+/*
+Given a double-precision number, payment, denoting an amount of money, use the NumberFormat class' getCurrencyInstance method to convert payment into the US, Indian, Chinese, and French currency formats. Then print the formatted values as follows:
+
+US: formattedPayment
+India: formattedPayment
+China: formattedPayment
+France: formattedPayment
+where formattedpayment is payment formatted according to the appropriate Locale's currency.
+
+Note: India does not have a built-in Locale, so you must construct one where the language is en (i.e., English).
+
+Input Format
+
+A single double-precision number denoting payment.
+
+Constraints
+0 <= payment <= 10^9
+Output Format
+
+On the first line, print US: u where u is payment formatted for US currency.
+On the second line, print India: i where i is payment formatted for Indian currency.
+On the third line, print China: c where c is payment formatted for Chinese currency.
+On the fourth line, print France: f, wheref is payment formatted for French currency.
+
+*/
+
+
+
+import java.util.*;
+import java.text.*;
+
+public class Solution {
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double payment = scanner.nextDouble();
+        scanner.close();
+        
+        // Write your code here.
+        String us= NumberFormat.getCurrencyInstance(Locale.US).format(payment);
+        String india= NumberFormat.getCurrencyInstance(new Locale("en", "IN")).format(payment);        
+        String china= NumberFormat.getCurrencyInstance(Locale.CHINA).format(payment);
+        String france= NumberFormat.getCurrencyInstance(Locale.FRANCE).format(payment);
+        
+        System.out.println("US: " + us);
+        System.out.println("India: " + india);
+        System.out.println("China: " + china);
+        System.out.println("France: " + france);
+    }
+}
