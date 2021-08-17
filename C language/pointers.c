379357a79ff53db1431bd3e58614664c33069920
pavithraravi07 <pavithraravisskv@gmail.com>
2021-08-17 21:20:15 +0530

    folder for C programs

---
+++ C language/pointers.c
@@ -0,0 +1,34 @@
+/*
+Task
+Complete the function void update(int *a,int *b). It receives two integer pointers, int* a and int* b. Set the value of a to their sum, and b to their absolute difference. There is no return value, and no return statement is needed.
+
+Input Format
+The input will contain two integers, a and b, separated by a newline.
+
+Output Format
+Modify the two values in place and the code stub main() will print their values.
+*/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void update(int *a,int *b) {
+    // Complete this function    
+    int sum,diff;
+    sum = *a+*b;
+    diff = abs(*a-*b);
+    *a = sum;
+    *b = diff;
+}
+
+int main() {
+    int a, b;
+    int *pa = &a, *pb = &b;
+    
+    scanf("%d %d", &a, &b);
+    update(pa, pb);
+    printf("%d\n%d", a, b);
+
+    return 0;
+}
