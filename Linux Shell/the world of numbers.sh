e417ab4210f6729f62d8300b460ade5d402f5b5b
pavithraravi07 <pavithraravisskv@gmail.com>
2021-08-27 19:19:07 +0530

    folder for shell scripts

---
+++ Linux Shell/the world of numbers.sh
@@ -0,0 +1,23 @@
+:'
+Given two integers, X and Y, find their sum, difference, product, and quotient.
+
+Input Format
+Two lines containing one integer each (X and Y, respectively).
+
+Output Format
+
+Four lines containing the sum, difference, product, and quotient, respectively.
+(While computing the quotient, print only the integer part.)
+'
+
+
+
+
+
+read num1;
+read num2;
+
+echo $((num1 + num2))
+echo $((num1 - num2))
+echo $((num1 * num2))
+echo $((num1 / num2))
