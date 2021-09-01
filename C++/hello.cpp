696e03a01bb0d394c865d6f735fe43792ad6f1ca
pavithraravi07 <pavithraravisskv@gmail.com>
2021-09-01 19:35:14 +0530

    folder for C++ program

---
+++ C++/hello.cpp
@@ -0,0 +1,19 @@
+/*
+Output Format
+Print  to stdout.
+
+Sample Output
+Hello, World!
+*/
+
+
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main()
+{
+    cout << "Hello, World!" << endl;
+    return 0;
+}
