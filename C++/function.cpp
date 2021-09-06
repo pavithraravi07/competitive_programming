e08cff7a14681f56c50d3926845ed6faef4db139
pavithraravi07 <pavithraravisskv@gmail.com>
2021-09-06 12:16:50 +0530

    folder for C++ programs

---
+++ C++/function.cpp
@@ -0,0 +1,36 @@
+/*
+Input Format
+Input will contain four integers - a, b, c, d, one per line.
+
+Output Format
+Return the greatest of the four integers.
+PS: I/O will be automatically handled.
+*/
+
+
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int max_of_four(int a, int b, int c, int d)
+{
+    int ans;
+    if (a > b && a > c && a > d)
+        ans = a;
+    else if (b > c && b > d)
+        ans = b;
+    else if (c > d)
+        ans = c;
+    else
+        ans = d;
+    return ans;
+}
+
+int main() {
+    int a, b, c, d;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+    int ans = max_of_four(a, b, c, d);
+    printf("%d", ans);
+    
+    return 0;
+}
