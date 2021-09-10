ae5c5bebf1971eaca8e670b85c65743d5df0d04b
pavithraravi07 <pavithraravisskv@gmail.com>
2021-09-10 18:00:52 +0530

    folder for C programs

---
+++ C language/defining_constants_using_macro_definition.c
@@ -0,0 +1,13 @@
+/*
+Fill in the missing lines of code to define the constant CREDIT so that the program prints 100 as output.
+*/
+
+
+
+#include <stdio.h>
+#define CREDIT 30
+int main()
+{
+    printf("%d",70+CREDIT);
+    return 0;
+}
