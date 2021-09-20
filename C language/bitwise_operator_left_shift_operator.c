2737b6d48feb5305d9951696abe046bb4c6165be
pavithraravi07 <pavithraravisskv@gmail.com>
2021-09-20 19:34:17 +0530

    folder for C programs

---
+++ C language/bitwise_operator_left_shift_operator.c
@@ -0,0 +1,16 @@
+/*
+Assign a value to the variable shift so that the program prints 32 as output.
+*/
+
+
+
+#include <stdio.h>
+
+int main()
+{
+    unsigned int a=2;
+    int shift;
+    shift=4;
+    printf("%d",a<<shift);
+    return 0;
+}
