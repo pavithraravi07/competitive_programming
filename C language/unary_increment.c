44f60013a5bb98807363e06cdb5afddaf51a5a31
Pavithra <58901707+pavithraravi07@users.noreply.github.com>
2021-11-06 10:26:34 +0530

    Add files via upload

---
+++ C language/unary_increment.c
@@ -0,0 +1,19 @@
+/*
+
+Initialize the variable number to a value so that the program below prints 50 as output.
+
+*/
+
+
+#include <stdio.h>
+
+int main()
+{
+    int number;
+
+    number=48;
+
+    number++;
+    printf("%d",++number);
+    return 0;
+}
