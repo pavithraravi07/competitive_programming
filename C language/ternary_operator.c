456c9ecb38e9dca0d40602fd994fe36d3cc7039a
Pavithra <58901707+pavithraravi07@users.noreply.github.com>
2021-11-09 19:01:43 +0530

    Add files via upload

---
+++ C language/ternary_operator.c
@@ -0,0 +1,21 @@
+/*
+
+The program below uses a ternary operator to print alpha or beta. Assign a value to the int variable inputvalue so that the program prints beta.
+
+Note: If the condition is false, the ternary operator will return beta.
+
+*/
+
+
+#include <stdio.h>
+
+int main()
+{
+    int inputvalue;
+
+    inputvalue=0;
+
+    printf(inputvalue ? "alpha":"beta");
+    return 0;
+}
+ 
