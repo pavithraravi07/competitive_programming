65e3499c5ddbedb402088925f7ae35278733f5b3
Pavithra <58901707+pavithraravi07@users.noreply.github.com>
2021-11-28 21:38:55 +0530

    Add files via upload

---
+++ C language/string_intro.c
@@ -0,0 +1,15 @@
+/*
+
+Fill in the code so that you declare and initialize the String variable continentname to Africa
+
+*/
+
+
+
+#include <stdio.h>    
+
+int main() {
+    char continentname[] = "Africa";
+    printf("%s",continentname);
+    return 0;
+}
