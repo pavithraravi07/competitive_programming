210b7f85d6a0944749215fe0c3473584fb850c4e
Pavithra <58901707+pavithraravi07@users.noreply.github.com>
2021-11-29 21:03:27 +0530

    Add files via upload

---
+++ C language/string_input.c
@@ -0,0 +1,16 @@
+/*
+
+Fill in the code so that you accept the name of the vegetable passed as input and store it in the char array vegetable.
+
+*/
+
+
+#include <stdio.h>    
+
+int main() {
+    char vegetable[50];
+    scanf("%s",vegetable);
+    printf("%s",vegetable);
+    return 0;
+}
+ 
