aa375ecec7f25fa868b1c5697c255c3cfa391f4e
Pavithra <58901707+pavithraravi07@users.noreply.github.com>
2022-01-02 19:03:39 +0530

    Add files via upload

---
+++ C language/order_of_passing_arguments.c
@@ -0,0 +1,14 @@
+/*
+
+Initialize the value of num so that the program prints 24 24 24
+
+*/
+
+
+#include <stdio.h>    
+
+int main() {
+    int num;
+    num=22;
+    printf("%d %d %d",num,++num,++num);
+}
