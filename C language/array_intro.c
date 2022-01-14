51946d6757f88544fcdf72da81b4fa8e23c0c562
Pavithra <58901707+pavithraravi07@users.noreply.github.com>
2022-01-14 19:49:33 +0530

    Add files via upload

---
+++ C language/array_intro.c
@@ -0,0 +1,17 @@
+/*
+
+Marks of 10 students are represented by the array studentMarks. Fill in the code to declare and assign value to an int variable fifthstudentindex so that the program below prints the fifth student's mark.
+
+*/
+
+
+
+#include <stdio.h>    
+
+int main() {  
+    int studentMarks[] = {10,20,30,40,50,60,70,80,90,95};   
+    int fifthstudentindex=4;
+    printf("%d",studentMarks[fifthstudentindex]);
+    return 0;
+}
+ 
