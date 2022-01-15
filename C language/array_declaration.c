c05bca6b797ae5adf62307376f6a21e79d317194
Pavithra <58901707+pavithraravi07@users.noreply.github.com>
2022-01-15 13:42:02 +0530

    Add files via upload

---
+++ C language/array_declaration.c
@@ -0,0 +1,16 @@
+/*
+
+Fill in the below code by declaring and initializing the Array ages, so that the program prints 30 as output.
+
+
+*/
+
+
+
+#include <stdio.h>    
+
+int main() {  
+    int ages[] = {22,25,30,35,40};
+    printf("%d",ages[2]);
+    return 0;
+}
