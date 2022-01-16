8cc15020d2f49a6312a00aabc327dbd8f26e7908
Pavithra <58901707+pavithraravi07@users.noreply.github.com>
2022-01-16 10:35:58 +0530

    Add files via upload

---
+++ C language/referencing_array_element.c
@@ -0,0 +1,22 @@
+/*
+
+Fill in the below code by setting all the Array element values of int Array price to 50.
+
+
+*/
+
+
+
+#include <stdio.h>    
+
+int main() {  
+    int price[5];
+    price[0]=50;
+    price[1]=50;
+    price[2]=50;
+    price[3]=50;
+    price[4]=50;
+    //You can also initialize using below code 
+    //price[0]=price[1]=price[2]=price[3]=price[4]=50;
+    printf("%d %d %d %d %d",price[0],price[1],price[2],price[3],price[4]);
+}
