54184041b342677345c4e338d87a31051f16ef84
Pavithra <58901707+pavithraravi07@users.noreply.github.com>
2022-01-17 12:46:33 +0530

    Add files via upload

---
+++ C language/default_initialization_values.c
@@ -0,0 +1,17 @@
+/*
+
+Fill in the below code by declaring int variables x,y and setting values for x and y so that the program below prints 90.
+
+*/
+
+
+
+#include <stdio.h>    
+
+int main() {  
+    int numbers[5]={10,20,80};
+    int x=0;
+    int y=2;
+    printf("%d",numbers[x]+numbers[y]);
+    return 0;
+}
