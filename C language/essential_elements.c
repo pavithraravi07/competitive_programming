f5d2c77a147be67f9b46574615871d5fc897f79f
Pavithra <58901707+pavithraravi07@users.noreply.github.com>
2022-02-04 15:03:49 +0530

    Add files via upload

---
+++ C language/essential_elements.c
@@ -0,0 +1,17 @@
+/*
+
+Fill in the code within the main function so that the program prints Brave as the output.
+
+Note: The output is case sensitive. So please ensure B is in upper case.
+
+*/
+
+
+
+#include <stdio.h>
+
+int main()
+{
+    printf("Brave");
+    return 0;
+}
