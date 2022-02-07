b340f0ab420068cdcd32cdf6b1e81f52c985d0e9
Pavithra <58901707+pavithraravi07@users.noreply.github.com>
2022-02-07 13:22:09 +0530

    Add files via upload

---
+++ C language/format_code_for_int_data_type.c
@@ -0,0 +1,18 @@
+/*
+
+Fill in the single line of code within the main function to print the value of the variable age.
+
+*/
+
+
+
+#include <stdio.h>
+
+int main()
+{
+    int age;
+    //The below function scanf accepts the input value and stores it in the variable age.
+    scanf("%d",&age);
+    printf("%d",age);
+    return 0;
+}
