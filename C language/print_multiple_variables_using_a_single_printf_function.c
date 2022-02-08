c7dbdbaefb88c71a5df90bc2148d90493038f73c
Pavithra <58901707+pavithraravi07@users.noreply.github.com>
2022-02-08 10:25:24 +0530

    Add files via upload

---
+++ C language/print_multiple_variables_using_a_single_printf_function.c
@@ -0,0 +1,17 @@
+/*
+
+Fill in the code within the main function to print the values of the three variables firstNumber, secondNumber, thirdNumber separated by a single hyphen (hyphen is nothing but the sign - ).
+
+As an example if the values of the three variables are 10, 20 and 30 the output must be 10-20-30
+
+*/
+
+
+#include <stdio.h>
+
+int main()
+{
+    int firstNumber=55,secondNumber=99,thirdNumber=11;
+    printf("%d-%d-%d",firstNumber,secondNumber,thirdNumber);
+    return 0;
+}-
