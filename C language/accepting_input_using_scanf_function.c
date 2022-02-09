5155dc3dc7abc46658dcf8bc58d127c7e3cb6b6e
Pavithra <58901707+pavithraravi07@users.noreply.github.com>
2022-02-09 18:06:18 +0530

    Add files via upload

---
+++ C language/accepting_input_using_scanf_function.c
@@ -0,0 +1,16 @@
+/*
+
+Fill in the code within the main function to accept and assign the value to the int variable numberOfParticipants using scanf function.   
+
+*/
+
+
+#include <stdio.h>
+
+int main()
+{
+    int numberOfParticipants;
+    scanf("%d", &numberOfParticipants);
+    printf("%d", numberOfParticipants);
+    return 0;
+}
