e199873a8f35b60895c57ec82e07d3e258b528a8
Pavithra <58901707+pavithraravi07@users.noreply.github.com>
2022-02-10 10:00:05 +0530

    Add files via upload

---
+++ C language/arithmetic_operators.c
@@ -0,0 +1,16 @@
+/*
+
+Fill in the code inside the main function to accept an integer as input, store it in variable weight. Then subtract 10 from the value of the variable weight and print the result as output.
+
+*/
+
+
+#include <stdio.h>
+
+int main()
+{
+    int weight;
+    scanf("%d", &weight);
+    printf("%d", weight-10);
+    return 0;
+}
