18ff0b0c7cfacaaa9adef72603549593ad7106ba
Pavithra <58901707+pavithraravi07@users.noreply.github.com>
2022-02-11 13:13:50 +0530

    Add files via upload

---
+++ C language/accept_numbers_and_print_their_sum_as_output.c
@@ -0,0 +1,18 @@
+/*
+
+Fill in the code inside the main function to accept three numbers as input and print their sum as output.
+
+*/
+
+
+#include <stdio.h>
+
+int main()
+{
+    int first,second,third;
+    scanf("%d", &first);
+    scanf("%d", &second);
+    scanf("%d", &third);
+    printf("%d", first+second+third);
+    return 0;
+}
