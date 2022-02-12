e1bcfab7006f6d0a14b774b3fef8a553c1cfd62f
Pavithra <58901707+pavithraravi07@users.noreply.github.com>
2022-02-12 21:33:09 +0530

    Add files via upload

---
+++ C language/modulo_operator.c
@@ -0,0 +1,17 @@
+/*
+
+Fill in the code inside the main function to print the remainder when the dividend is divided by the divisor.
+
+*/
+
+
+#include <stdio.h>
+
+int main()
+{
+    int dividend;
+    int divisor;
+    scanf("%d%d",&dividend,&divisor);
+    printf("%d", dividend%divisor);
+    return 0;
+}
