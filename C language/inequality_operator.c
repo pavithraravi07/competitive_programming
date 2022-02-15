a655f2a0b67758db15cd210527a2f58187d581b0
Pavithra <58901707+pavithraravi07@users.noreply.github.com>
2022-02-15 10:00:23 +0530

    Add files via upload

---
+++ C language/inequality_operator.c
@@ -0,0 +1,16 @@
+/*
+
+Fill in the code inside the main function to print 1 if the value of the variable x is NOT equal to 100. The program must print 0 if the value of the variable x is equal to 100.
+
+*/
+
+
+#include <stdio.h>
+
+int main()
+{
+    int x;
+    scanf("%d",&x);
+    printf("%d",x!=100);
+    return 0;
+}
