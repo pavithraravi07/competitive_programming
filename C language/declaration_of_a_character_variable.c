e3273332bbc23e978713b67c34b38866028d08e4
Pavithra <58901707+pavithraravi07@users.noreply.github.com>
2022-02-16 10:03:15 +0530

    Add files via upload

---
+++ C language/declaration_of_a_character_variable.c
@@ -0,0 +1,14 @@
+/*
+
+Declare a character variable gender and initialize it to 'M', so that the below program compiles and prints the output.
+
+*/
+
+
+#include <stdio.h>
+
+int main()
+{
+    char gender = 'M';
+    printf("%c",gender);
+}
