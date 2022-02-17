65c45cbea407b5407e9dfd1799e10fa291b8fe70
Pavithra <58901707+pavithraravi07@users.noreply.github.com>
2022-02-17 11:25:27 +0530

    Add files via upload

---
+++ C language/accepting_character_as_input.c
@@ -0,0 +1,17 @@
+/*
+
+Steven has written the program below with the char variable gender but does not know how to accept input values for it. 
+Help Steven by inserting the missing lines of code to accept the input value.
+
+*/
+
+
+#include <stdio.h>
+
+int main()
+{
+    char gender;
+    scanf("%c",&gender);
+    printf("%c",gender);
+    return 0;
+}
