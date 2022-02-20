2bb4e7f92cb3ff1551b4a4d3f0fb127c9d620370
Pavithra <58901707+pavithraravi07@users.noreply.github.com>
2022-02-20 10:47:35 +0530

    Add files via upload

---
+++ Java/essential_elements.java
@@ -0,0 +1,12 @@
+/*
+
+Modify the Java program given below to print courage as the output (Currently the program prints coward as the output).
+
+*/
+
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("courage");
+    }
+}
