298af7e9d54516f65f490d224c100cbfd4040d94
Pavithra <58901707+pavithraravi07@users.noreply.github.com>
2022-02-21 18:21:57 +0530

    Add files via upload

---
+++ Java/printing_output.java
@@ -0,0 +1,13 @@
+/*
+
+Amit wants to write a Java program which will print a line Wonderful Job to appreciate his kid (when ever the kid does a smart stuff).
+Please help Amit by inserting the missing lines of code in the program below.
+Note: The output is case sensitive. Please note W in Wonderful and J in Job are in upper case.
+
+*/
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Wonderful Job");
+    }//end of main method
+}
