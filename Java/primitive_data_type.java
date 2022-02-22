17aa285744cc4da1a903b31446ef1629f4081bb4
Pavithra <58901707+pavithraravi07@users.noreply.github.com>
2022-02-22 19:03:26 +0530

    Add files via upload

---
+++ Java/primitive_data_type.java
@@ -0,0 +1,21 @@
+/*
+
+Fill in the missing lines of code to declare x, y and z as int variables, 
+so that the below program prints the sum of x, y and z
+
+*/
+
+
+
+public class Hello {
+
+    public static void main(String[] args) {
+
+        int x, y, z;
+        x = 10;
+        y = 200;
+        z = 30;
+        System.out.println(x + y + z);
+
+    }
+}
