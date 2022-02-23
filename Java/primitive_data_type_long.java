91cc4ed34eab4f843e98f0386dbe7dafe55dd62a
Pavithra <58901707+pavithraravi07@users.noreply.github.com>
2022-02-23 09:59:47 +0530

    Add files via upload

---
+++ Java/primitive_data_type_long.java
@@ -0,0 +1,15 @@
+/*
+
+Fill in the missing lines of code to assign the value 998877665544332211 to the long variable bignum.
+
+*/
+
+
+public class Hello {
+
+    public static void main(String[] args) {
+        long bignum;
+        bignum = 998877665544332211L;
+        System.out.println(bignum);
+    }
+}
