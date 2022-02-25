e132b01355ba0e5149e28b159630f4f9b554b8e1
Pavithra <58901707+pavithraravi07@users.noreply.github.com>
2022-02-25 15:44:13 +0530

    Add files via upload

---
+++ Java/primitive_data_type_char.java
@@ -0,0 +1,14 @@
+/*
+
+Fill in the missing lines of code to declare a char variable called gender and assign the value M to it.
+
+*/
+
+
+public class Hello {
+
+    public static void main(String[] args) {
+        char gender = 'M';
+        System.out.println(gender);
+    }
+}
