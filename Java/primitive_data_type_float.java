499f8e072f160b579dbdf596c0aee4eb8631957f
Pavithra <58901707+pavithraravi07@users.noreply.github.com>
2022-02-26 11:53:05 +0530

    Add files via upload

---
+++ Java/primitive_data_type_float.java
@@ -0,0 +1,15 @@
+/*
+
+Fill in the missing lines of code to declare a float variable called interest and assign the value 2000.34 to it.
+
+*/
+
+
+
+public class Hello {
+
+    public static void main(String[] args) {
+        float interest=2000.34f;
+        System.out.println(interest);
+    }
+}
