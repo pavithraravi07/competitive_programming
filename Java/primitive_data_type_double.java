a0ee98536b235cbb1184003fc376d6da0ee12554
Pavithra <58901707+pavithraravi07@users.noreply.github.com>
2022-02-27 13:19:05 +0530

    Add files via upload

---
+++ Java/primitive_data_type_double.java
@@ -0,0 +1,14 @@
+/*
+
+Fill in the missing lines of code to declare a double variable called revenue and assign the value 12345.12 to it.
+
+*/
+
+
+public class Hello {
+
+    public static void main(String[] args) {
+        double revenue=12345.12d;
+        System.out.println(revenue);
+    }
+}
