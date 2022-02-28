20d2690321d7dbe768b22aef8ce4543dc0da0758
Pavithra <58901707+pavithraravi07@users.noreply.github.com>
2022-02-28 17:33:24 +0530

    Add files via upload

---
+++ Java/accepting_input_using_scanner_int_data_type.java
@@ -0,0 +1,19 @@
+/*
+
+Fill in the missing lines of code so that the program accepts two int data type values and prints their sum as the output.
+
+*/
+
+
+
+import java.util.Scanner;
+
+public class Hello {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+        System.out.println(num1+num2);
+    }
+}
