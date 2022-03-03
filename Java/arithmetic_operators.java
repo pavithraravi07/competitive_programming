ef3bed90e3f733c03189d6ea72cf1379e94c76bc
Pavithra <58901707+pavithraravi07@users.noreply.github.com>
2022-03-03 18:30:03 +0530

    Add files via upload

---
+++ Java/arithmetic_operators.java
@@ -0,0 +1,18 @@
+/*
+
+Fill in the missing lines of code so that the program prints the quotient when M is divided by N.
+
+*/
+
+
+import java.util.Scanner;
+
+public class Hello {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int M=sc.nextInt();
+        int N=sc.nextInt();
+        System.out.println(M/N);
+    }
+}
