ab1dcf0c01dd35786bff023b4d8e8673997f3f03
Pavithra <58901707+pavithraravi07@users.noreply.github.com>
2022-03-07 10:27:05 +0530

    Add files via upload

---
+++ Java/unary_operator.java
@@ -0,0 +1,16 @@
+/*
+
+Fill in the missing lines of code to declare and initialize the value of int variable k so that the program prints 5 followed by 7 as the output.
+
+*/
+
+
+
+public class Hello {
+
+    public static void main(String[] args) {
+        int k = 5;
+        System.out.println(k++);
+        System.out.println(++k);
+    }
+}
