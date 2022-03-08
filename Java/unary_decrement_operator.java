d6fa951de56850caa430a3946cb4867455876ba6
Pavithra <58901707+pavithraravi07@users.noreply.github.com>
2022-03-08 10:28:28 +0530

    Add files via upload

---
+++ Java/unary_decrement_operator.java
@@ -0,0 +1,13 @@
+/*
+
+Fill in the missing line of code to declare and initialize counter so that the program prints -2 as the output.
+
+*/
+
+
+public class Hello {
+    public static void main(String[] args) {
+                   int counter = 0;
+        System.out.println(--counter+counter++);
+    }
+}
