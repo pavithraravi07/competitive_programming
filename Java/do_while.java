560b4ad9dd4896ba8b1d57fdf2ba6c9f635c7954
Pavithra <58901707+pavithraravi07@users.noreply.github.com>
2022-03-16 13:03:52 +0530

    Add files via upload

---
+++ Java/do_while.java
@@ -0,0 +1,22 @@
+/*
+Complete the below program by specifying while condition as per the below rules.
+- Print only the input number if the number is odd.
+- If the input number is even, print the input number and the number which is lesser by one (which is odd).
+
+Example:
+If the input is 11 only 11 is printed.
+If the input is 56, both 56 55 are printed.
+*/
+
+
+import java.util.Scanner;
+public class Hello {
+public class Hello {
+   public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        do {
+            System.out.println(number);
+        } 
+        while(number-- %2 == 0);
+    }
+}
