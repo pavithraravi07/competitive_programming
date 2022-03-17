40d7b2519686e510e0eae98b26a2d4dc5dfa80be
Pavithra <58901707+pavithraravi07@users.noreply.github.com>
2022-03-17 13:53:00 +0530

    Add files via upload

---
+++ Java/break.java
@@ -0,0 +1,17 @@
+/*
+
+The program below prints from 2 to 20.
+With the help of break statement inside for loop, ensure the program prints only till 10.
+
+*/
+
+
+public class Hello {
+public static void main(String[] args) {
+    for(int ctr=2;ctr<=20;ctr+=2){
+        System.out.println(ctr);
+        if(ctr==10)
+            break;
+        }
+    }
+}
