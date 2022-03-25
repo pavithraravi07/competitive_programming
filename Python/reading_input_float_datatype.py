76eda49ad0bdfd3d7a908641bcc52087eddd06b4
Pavithra <58901707+pavithraravi07@users.noreply.github.com>
2022-03-25 13:42:10 +0530

    Add files via upload

---
+++ Python/reading_input_float_datatype.py
@@ -0,0 +1,12 @@
+# Insert the missing lines of code so that the program accepts two float values and prints their difference as the output.
+
+# Examples:
+# - If the input values are 5.5 and 7.5, then the output must be -2.0
+# - If the input values are 5.5 and 2.5, then the output must be 3.0
+
+
+
+
+x = float(input())
+y = float(input())
+print (x-y)
