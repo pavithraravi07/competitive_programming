dc54a0592cc78040b691e6d4bf899fda2634bae9
Pavithra <58901707+pavithraravi07@users.noreply.github.com>
2022-03-26 11:16:03 +0530

    Add files via upload

---
+++ Python/rounding_float_value.py
@@ -0,0 +1,12 @@
+# Insert the missing lines of code so that the program accepts two float values and 
+# prints their sum rounded to 3 decimal places.
+
+# Examples:
+# - If the input values are 5.5156 and 7.5002, then the output must be 13.016
+# - If the input values are 1.1123 and 4.0011, then the output must be 5.113
+
+
+
+x = float(input())
+y = float(input())
+print (round(x+y,3))
