0f99289271fc2744f35dc1c5e0c0e0542463c637
Pavithra <58901707+pavithraravi07@users.noreply.github.com>
2022-03-28 11:28:07 +0530

    Add files via upload

---
+++ Python/strings_slice_operator.py
@@ -0,0 +1,6 @@
+# Complete the program so that it prints the initial 5 characters of the input string value. 
+# (Assume that the length of the input string is more than 5).
+
+
+inputstring = input()
+print(inputstring[0:5])
