9717efa40bfa81225d3771cb662e8847834ee2e4
Pavithra <58901707+pavithraravi07@users.noreply.github.com>
2022-04-01 10:40:35 +0530

    Add files via upload

---
+++ Python/string_concatenation.py
@@ -0,0 +1,8 @@
+# Complete the program so that it concatenates the string values in the variables first, second and 
+# third and prints the combined string value as the output.
+
+
+first = input()
+second = input()
+third = input()
+print(first.strip()+second.strip()+third.strip())
