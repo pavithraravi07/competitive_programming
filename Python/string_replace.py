f3ca47f2b7e4439f9196cb7ba7e6b48596473f31
Pavithra <58901707+pavithraravi07@users.noreply.github.com>
2022-04-04 10:48:52 +0530

    Add files via upload

---
+++ Python/string_replace.py
@@ -0,0 +1,7 @@
+# Complete the program so that accepts an input string value and replaces j with y 
+# and prints the replaced value as the output.
+
+
+
+inputstring = input()
+print (inputstring.replace("j","y"))
