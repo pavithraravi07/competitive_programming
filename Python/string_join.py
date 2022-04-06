682234558f0849b047bbfb16f42d6d58e68cf060
Pavithra <58901707+pavithraravi07@users.noreply.github.com>
2022-04-06 10:13:29 +0530

    Add files via upload

---
+++ Python/string_join.py
@@ -0,0 +1,8 @@
+# Complete the program so that accepts an input string value and separates the characters in the 
+# string using colon.
+# If the input is aeiou, the output must be a:e:i:o:u
+
+
+inputstring = input()
+sep=":"
+print (sep.join(inputstring))
