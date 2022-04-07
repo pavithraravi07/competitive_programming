8f20befafe609e8d9c40bbe46adb8991f1923e76
Pavithra <58901707+pavithraravi07@users.noreply.github.com>
2022-04-07 10:52:21 +0530

    Add files via upload

---
+++ Python/list.py
@@ -0,0 +1,7 @@
+# Complete the program to declare a list called cities with three string values as 
+# Chennai, Delhi and Mumbai.
+
+
+cities = ["Chennai","Delhi","Mumbai"]
+for city in cities:
+	print (city)
