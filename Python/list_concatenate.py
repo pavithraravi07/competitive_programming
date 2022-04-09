94d87c0471e8e2c526e8f88c53acd096605fdbf2
Pavithra <58901707+pavithraravi07@users.noreply.github.com>
2022-04-09 18:57:24 +0530

    Add files via upload

---
+++ Python/list_concatenate.py
@@ -0,0 +1,10 @@
+# Complete the program to combine the items in list1 and list2 into list3.
+
+
+
+
+list1 = list(input().split())
+list2 = list(input().split())
+list3 = list1+list2
+for item in list3:
+    print (item, end=" ")
