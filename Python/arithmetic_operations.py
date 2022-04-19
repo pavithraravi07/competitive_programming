ced4374a31a004c6f477fd702dd0789801297abb
Pavithra <58901707+pavithraravi07@users.noreply.github.com>
2022-04-19 17:50:02 +0530

    Add files via upload

---
+++ Python/arithmetic_operations.py
@@ -0,0 +1,11 @@
+# Fill in the missing lines of code to initialize variables a, b, c, d so that the program 
+# prints 185.0 as the output.
+
+
+
+a = 200
+b = 2
+c = 20
+d = 100
+e = 4
+print (a+c/b-d/e)
