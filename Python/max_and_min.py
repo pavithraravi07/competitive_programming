dc1467541e12c36fc59221b9f7c27acf4b085e73
Pavithra <58901707+pavithraravi07@users.noreply.github.com>
2022-04-24 11:20:36 +0530

    Add files via upload

---
+++ Python/max_and_min.py
@@ -0,0 +1,19 @@
+# Fill in the missing lines of code so that the program 
+# accepts the values for three sides of a triangle and 
+# prints the value of the smallest side.
+
+# Example Input/Output:
+# Input:
+# 5
+# 10
+# 7
+
+# Output:
+# 5
+
+
+
+a = int(input())
+b = int(input())
+c = int(input())
+print (min(a,b,c))
