10dde979377e711531bb3941a98cb7974ffe1695
Pavithra <58901707+pavithraravi07@users.noreply.github.com>
2022-04-27 14:37:46 +0530

    Add files via upload

---
+++ Python/string_triple_quotes.py
@@ -0,0 +1,17 @@
+# Fill in the missing lines of code so that the program prints the output given below.
+# Apple Banana
+# Carrot
+# Doll Elephant
+# Fish Gateway
+# Hotspot Internet
+# Jack
+
+
+
+strval = '''Apple Banana
+Carrot
+Doll Elephant
+Fish Gateway
+Hotspot Internet
+Jack'''
+print (strval)
