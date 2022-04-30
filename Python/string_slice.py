1d3b7019554fcf5e66d6fda81ef5a0c8463c4efc
Pavithra <58901707+pavithraravi07@users.noreply.github.com>
2022-04-30 19:09:56 +0530

    Add files via upload

---
+++ Python/string_slice.py
@@ -0,0 +1,15 @@
+# A string value is passed as the input to the program. 
+# Fill in the missing lines of code so that the program 
+# prints the characters in the even position in the input string value.
+
+# Example Input/Output:
+# Input:
+# a1b2c3d4e5
+
+# Output:
+# 12345
+
+
+
+strval = input()
+print (strval[1::2])
