dfa7db4eb57e84850e877e39b4716d467cf72ab3
Pavithra <58901707+pavithraravi07@users.noreply.github.com>
2022-05-06 14:31:19 +0530

    Add files via upload

---
+++ Python/dictionary_get_method.py
@@ -0,0 +1,14 @@
+# Fill in the missing lines of code, so that the output of the program is
+# Apple
+# NOTPRESENT
+# NOTPRESENT
+# Eagle
+
+
+
+defaultvalue = "NOTPRESENT"
+letterword = {"A":"Apple", "E":"Eagle", "S":"Snake"}
+print (letterword.get("A",defaultvalue))   
+print (letterword.get("B",defaultvalue))  
+print (letterword.get("R",defaultvalue))   
+print (letterword.get("E",defaultvalue))
