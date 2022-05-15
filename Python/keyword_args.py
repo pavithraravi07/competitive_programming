f9920a07e9f0d66cdccde3f1fc735c887c43762d
Pavithra <58901707+pavithraravi07@users.noreply.github.com>
2022-05-15 12:45:20 +0530

    Add files via upload

---
+++ Python/keyword_args.py
@@ -0,0 +1,20 @@
+# Fill in the missing lines of code so that the program prints the two input strings
+# separated by a space.
+
+# Example Input/Output:
+# Input:
+# Arnold
+# Schwarzenegger
+
+# Output:
+# Arnold Schwarzenegger
+
+
+def printName(firstname, lastname):
+    print (firstname + " " + lastname)
+
+
+str1 = input()
+str2 = input()
+printName(firstname=str1, lastname=str2)
+ 
