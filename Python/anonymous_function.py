54d0b426c6119e4f15119e018d53992ecd2f0527
Pavithra <58901707+pavithraravi07@users.noreply.github.com>
2022-05-16 11:01:59 +0530

    Add files via upload

---
+++ Python/anonymous_function.py
@@ -0,0 +1,13 @@
+# Fill in the missing lines of code so that the output of the program is
+# 121
+# 36
+# 4
+
+
+a = 10
+b = 5
+c = 1
+equate = lambda x:x**2 + 2*x +1
+print (equate(a))
+print (equate(b))
+print (equate(c))
