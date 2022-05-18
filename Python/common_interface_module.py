7fe5a206ce7e63229dd5463c1e671d76250be09a
Pavithra <58901707+pavithraravi07@users.noreply.github.com>
2022-05-18 11:18:51 +0530

    Add files via upload

---
+++ Python/common_interface_module.py
@@ -0,0 +1,22 @@
+# DiwaliDiscount.py is as below.
+
+# def getDiwaliDiscount():
+#    return 10
+# NewYearDiscount.py is as below
+
+# def getNewYearDiscount():
+#    return 20
+# Hello.py is as below
+
+# from Discount import *
+
+# print (getNewYearDiscount())
+# print (getDiwaliDiscount())
+# Fill in the missing lines of code for Discount.py so that Hello.py
+# when executed prints the following output.
+# 20
+# 10
+
+
+from DiwaliDiscount import getDiwaliDiscount
+from NewYearDiscount import getNewYearDiscount
