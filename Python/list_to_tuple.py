bf8ca75b981669f1161e532d11c5b18b41631ccb
Pavithra <58901707+pavithraravi07@users.noreply.github.com>
2022-06-03 15:31:12 +0530

    Add files via upload

---
+++ Python/list_to_tuple.py
@@ -0,0 +1,9 @@
+# Complete the program so that the output is 1 2 3 4 5 6 7 10
+
+
+values = [1,2,3,4,5]
+values.append(6)
+values.append(7)
+values.append(10)
+for v in values:
+    print (v, end=" ")
