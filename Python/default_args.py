080a0fa64bf4542025d46970d7a993d9bc866cb6
Pavithra <58901707+pavithraravi07@users.noreply.github.com>
2022-06-04 13:21:54 +0530

    Add files via upload

---
+++ Python/default_args.py
@@ -0,0 +1,12 @@
+# Fill in the missing lines of code so that the program prints the following output.
+
+# 500
+# 600
+
+
+def printSum(x,y,z=200):
+    print (x+y+z)
+    
+printSum(120,180)
+printSum(100,200,300)
+ 
