9f0ba8671f738c8af701c74056430c7e3bbdebdc
Pavithra <58901707+pavithraravi07@users.noreply.github.com>
2022-06-10 11:55:47 +0530

    Add files via upload

---
+++ Python/user_defined_function.py
@@ -0,0 +1,10 @@
+# Fill in the missing lines of code so that the program
+# prints four times the input value as the output.
+
+
+def fourtimes(x):
+    return 4*x
+
+num = int(input())
+print (fourtimes(num))
+ 
